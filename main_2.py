5d41f11e3f0f4ed06056d8b69716e66ee82f3976
polesonair <88142197+polesonair@users.noreply.github.com>
2021-10-11 13:52:57 +0300

    Add files via upload
    Select

---
+++ main_2.py
@@ -0,0 +1,55 @@
+import sqlalchemy
+#import psycopg2
+
+def seconds_min(seconds_in):
+    minute = seconds_in // 60
+    sec = seconds_in % 60
+    sec = '{:02}'.format(sec)
+    return (minute, sec)
+
+db = 'postgresql://polesonair:poles@localhost:5432/05102021'
+engine = sqlalchemy.create_engine(db)
+connection = engine.connect()
+
+sel = connection.execute('SELECT name_of_album FROM albums WHERE release_date = 2018.').fetchall()
+print('Альбомы выпущенные в 2018 году:')
+for i in sel:
+    print(*i)
+
+print()
+print('Название и продолжительность самого длительного трека.')
+max_time = connection.execute('SELECT MAX(time) FROM Track ').fetchone()
+name_max_time = connection.execute(f'SELECT track_name FROM Tracklist where time = {max_time[0]}').fetchone()
+
+max_time = sec_min(max_time[0])  # секунды в минуты
+
+print(f'Трек: {name_max_time[0]}, длительность: {max_time[0]}:{max_time[1]}')
+print()
+
+print('Название треков, продолжительность которых не менее 3,5 минуты.')
+long_tracks = connection.execute('SELECT time, name_of_track FROM Tracklist WHERE time >= 210').fetchall()
+
+for i in long_tracks:
+    track_time = sec_min(i[0])
+    print(f'Трек: {i[1]}, длительность: {track_time[0]}:{track_time[1]}')
+print()
+
+print('Названия сборников, вышедших в период с 2018 по 2020 год включительно.')
+sel = connection.execute('SELECT mix_name, year FROM Mix WHERE year BETWEEN 2018 AND 2020').fetchall()
+for i in sel:
+    print(f'Альбом: {i[0]}, год выпуска: {i[1]}')
+print()
+
+print('Исполнители, чье имя состоит из 1 слова.')
+sel = connection.execute('SELECT name_of_artist FROM artist').fetchall()
+for i in sel:
+    if ' ' not in i[0].strip():
+        print(i[0])
+print()
+
+print('Название треков, которые содержат слово "я"/"me".')
+sel = connection.execute("""SELECT name_of_track FROM Tracklist WHERE track_name iLIKE 'me %%'
+ OR track_name iLIKE '%% me %%' OR track_name iLIKE '%% me' OR track_name iLIKE 'я %%'
+ OR track_name iLIKE '%% я %%' OR track_name iLIKE '%% я'""").fetchall()
+for i in sel:
+    print(i[0])
